dd8789380193d179418ff8c11489c58fe426f3b5
florian <martinflorian2@gmail.com>
2022-01-11 16:13:11 +0100

    forgot something

--- script.sh
+++ script.sh
@@ -14,12 +14,11 @@ echo "******** SCRIPT STARTING, GET ROLL MA MAN *********"
 
 mkdir backup
 
-cp -r * var/www/html/ backup/
+cp -avr var/www/html/ backup/
 
 mysqldump -u flo -p wp_cda > backup/data-dump.sql
 
-zip -r data.zip backup
+tar -r data.zip backup
 
 # pour mettre fin a notre script
 exit
-
