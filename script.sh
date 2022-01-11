5755f60f552df496e47ba9e9ee26cd582c978923
florian <martinflorian2@gmail.com>
2022-01-11 15:07:19 +0100

    script v1

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
-# TODO
+# définit le code qui suivra en script
+#! /bin/sh
+
+# script.sh va creer une sauvegarde de notre app
+
+# accorder les droits d'execution
+# chmod +x script.sh
+
+echo "début de la sauvegarde de notre application"
+
+mkdir backup
+
+cp -r * var/www/html/ backup/
+
+mysqldump -u flo -p wp_cda > backup/data-dump.sql
+
+zip -r data.zip backup
+
+# pour mettre fin a notre script
+exit
